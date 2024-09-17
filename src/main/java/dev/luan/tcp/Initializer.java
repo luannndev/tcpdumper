0a809aacf0c2aadef342dca416d3b7007664a8cf
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-17 10:18:21 +0200

    Add Initializer class and enhance TCPDumper functionality
    Introduce Initializer class to serve as the main entry point of the application. Enhance TCPDumper by adding startup graphics, file handling, network interface monitoring, and notification support. Consolidate bandwidth filtering and TCP dump triggering logic.

---
+++ src/main/java/dev/luan/tcp/Initializer.java
@@ -0,0 +1,9 @@
+package dev.luan.tcp;
+
+public class Initializer {
+
+    public static void main(String[] args) {
+        TCPDumper tcpDumper = new TCPDumper();
+    }
+
+}
--- src/main/java/dev/luan/tcp/TCPDumper.java
+++ src/main/java/dev/luan/tcp/TCPDumper.java
@@ -2,27 +2,165 @@ package dev.luan.tcp;
 
 import dev.luan.tcp.config.Config;
 
+import java.io.*;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class TCPDumper {
+
 
     private Config config;
     private int triggerScale;
     private long lastTrigger;
     private DateTimeFormatter dateTimeFormatter;
+    private WebhookManager webhookManager;
 
 
     public TCPDumper() {
         this.lastTrigger = 0;
-        this.dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+        this.dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy-HH:mm:ss");
         this.startProcess();
     }
 
     private void startProcess() {
+        this.printStartupGraphic();
+        try {
+            new File("dumps/").mkdirs();
+            this.config = Config.fromFile(new File("config.json"));
+            this.webhookManager = new WebhookManager(this);
+            this.triggerScale = this.checkUnitScale(this.config.unitToTrigger);
+            Thread.sleep(1000);
+            System.out.println("Startup Completed");
+            String[] args = new String[]{"/bin/bash", "-c", "nload devices " + this.config.networkInterface, "with", "args"};
+            Process process = new ProcessBuilder(args).start();
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            try {
+                this.print(process, reader);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void print(Process process, BufferedReader console) throws IOException {
+        String line;
+        while ((line = console.readLine()) != null) {
+            if(!process.isAlive()) {
+                process.destroy();
+            }
+            if(line.contains("Curr:")) {
+                String[] parts = line.split("Curr: ");
+                String[] partsParts = parts[1].split("/s");
+                System.out.println(partsParts[0] + "/s");
+                this.filter(partsParts[0]);
+            };
+        }
+    }
+
+    private void printDump(Process process, BufferedReader console) throws IOException {
+        String line;
+        while ((line = console.readLine()) != null) {
+            if(!process.isAlive()) {
+                process.destroy();
+            }
+            //System.out.println(line);
+        }
+    }
+
+    private void filter(String string) throws IOException {
+        String[] bandwidth = string.split(" ");
+        String unit = bandwidth[1];
+        double amount = Double.parseDouble(bandwidth[0]);
+
+        if(amount >= this.config.bandWidth) {
+            if(this.checkUnitScale(unit) >= this.triggerScale && (System.currentTimeMillis() - this.lastTrigger) > this.config.cooldownToNextDumpMS) {
+                this.lastTrigger = System.currentTimeMillis();
+                String poT = this.getDateTime();
+                System.out.println("TRIGGERED AT " + poT + " RUNNING FOR " + this.config.tcpDumpDuration);
+                this.triggerTCPDump(poT, amount + " " + unit);
+            }
+        }
+    }
+
+    private int checkUnitScale(String unit) {
+        switch (unit.toLowerCase(Locale.ROOT)) {
+            case "bit":
+                return 0;
+            case "kbit":
+                return 1;
+            case "mbit":
+                return 2;
+            case "gbit":
+                return 3;
+        }
+        return 0;
+    }
+
+    private void triggerTCPDump(String poT, String magnitude) throws IOException {
+        ThreadHandler.startExecute(() -> {
+            String[] args;
+            if(this.config.maxPacketsPerDump < 1) {
+                args = new String[]{"/bin/bash", "-c", "timeout " + this.config.tcpDumpDuration + " tcpdump -i " + this.config.networkInterface + " -n -l " + this.config.additionalParameters + " -w dumps/" + poT + ".pcap", "with", "args"};
+            } else {
+                args = new String[]{"/bin/bash", "-c", "tcpdump -i " + this.config.networkInterface + " -n -l -c " + this.config.maxPacketsPerDump + " " + this.config.additionalParameters + " -w dumps/" + poT + ".pcap", "with", "args"};
+            }
+            System.out.println(args[2]);
+            try {
+                Process process = new ProcessBuilder(args).start();
+                InputStream inputStream = process.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                printDump(process, reader);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        this.webhookManager.sendDiscordNotifications(poT, magnitude);
+        this.webhookManager.sendTelegramNotification(poT, magnitude);
     }
 
     public Config getConfig() {
         return config;
+    }
+
+    private void printStartupGraphic() {
+        System.out.println("" +
+                "@@@@@@@   @@@@@@@  @@@@@@@      @@@@@@@   @@@  @@@  @@@@@@@@@@   @@@@@@@   @@@@@@@@  @@@@@@@ \n" +
+                "@@@@@@@  @@@@@@@@  @@@@@@@@     @@@@@@@@  @@@  @@@  @@@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@\n" +
+                "  @@!    !@@       @@!  @@@     @@!  @@@  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!       @@!  @@@\n" +
+                "  !@!    !@!       !@!  @!@     !@!  @!@  !@!  @!@  !@! !@! !@!  !@!  @!@  !@!       !@!  @!@\n" +
+                "  @!!    !@!       @!@@!@!      @!@  !@!  @!@  !@!  @!! !!@ @!@  @!@@!@!   @!!!:!    @!@!!@! \n" +
+                "  !!!    !!!       !!@!!!       !@!  !!!  !@!  !!!  !@!   ! !@!  !!@!!!    !!!!!:    !!@!@!  \n" +
+                "  !!:    :!!       !!:          !!:  !!!  !!:  !!!  !!:     !!:  !!:       !!:       !!: :!! \n" +
+                "  :!:    :!:       :!:          :!:  !:!  :!:  !:!  :!:     :!:  :!:       :!:       :!:  !:!\n" +
+                "   ::     ::: :::   ::           :::: ::  ::::: ::  :::     ::    ::        :: ::::  ::   :::\n" +
+                "   :      :: :: :   :           :: :  :    : :  :    :      :     :        : :: ::    :   : :\n" +
+                "                                                                                             \n" +
+                "TCP Dumper by luannndev");
+    }
+
+    private String getDateTime() throws IOException {
+        /*OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://worldtimeapi.org/api/timezone/" + this.config.timeZone)
+                .method("GET", null)
+                .build();
+        Response response = client.newCall(request).execute();
+        if(response.isSuccessful()) {
+            if(response.body() != null) {
+                String data = Objects.requireNonNull(response.body()).string();
+                JSONObject jsonObject = new JSONObject(data);
+                response.close();
+
+                return jsonObject.getString("datetime");
+            }
+        }
+         */
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return this.dateTimeFormatter.format(localDateTime);
     }
 }
